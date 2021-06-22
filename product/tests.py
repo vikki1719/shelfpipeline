5ac8cefabc2f21fd5abd7e59659863dfbef52073
VIGNESH P <63351814+vikki1719@users.noreply.github.com>
2021-06-22 21:51:30 +0530

    Update tests.py

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+# django 2.x
+from django.urls import reverse
+
+class urlss(TestCase):
+    def test_home(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
